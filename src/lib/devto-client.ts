09a62a282aa0bc94b2ffa4522a8d16b1d41a46be
Bram Borggreve <borggreve@gmail.com>
2020-07-08 07:26:46 -0500

    feat: create the DEV client

---
+++ src/lib/devto-client.ts
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+export class DevtoClient {
+  private readonly baseUrl = 'https://dev.to/api';
+
+  private readonly headers: { 'api-key': string };
+
+  constructor(private readonly apiKey: string) {
+    this.headers = { 'api-key': this.apiKey };
+  }
+
+  get(url: string) {
+    return fetch(this.baseUrl + url, { headers: this.headers }).then((res) =>
+      res.json()
+    );
+  }
+
+  async getAccountArticles() {
+    const result = await this.get('/articles/me/all');
+
+    if (!result) {
+      throw new Error(`Error retrieving articles`);
+    }
+    return result;
+  }
+}
